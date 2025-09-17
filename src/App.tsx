90b236b0584bea799fc876214b98ad6d95ce0ae2
mahdee10 <120922767+mahdee10@users.noreply.github.com>
2025-09-17 17:43:01 +0200

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { getRuntimeConfig } from "./runtimeConfig";
+// import { getRuntimeConfig } from "./runtimeConfig";
 
 export default function App() {
-  const { orgName, apiBaseUrl } = getRuntimeConfig();
+  // const { orgName, apiBaseUrl } = getRuntimeConfig();
   return (
     <div style={{ padding: 16 }}>
-      <h1>{orgName} Console</h1>
-      <small>API: {apiBaseUrl}</small>
+      {/* <h1>{orgName} Console</h1> */}
+      {/* <small>API: {apiBaseUrl}</small> */}
+      <h1 className="text-red-500 text-5xl">hello</h1>
     </div>
   );
 }
